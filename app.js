4cc7bcaed5666c7aa94d43ea4cc69c9d6498da3a
Faiaz Hossain <faiazhossain10@gmail.com>
2024-01-24 11:54:09 +0600

    fix: server seperate

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+
 const app = express();
 
 const tourRouter = require('./routes/tourRoutes');
@@ -8,8 +9,4 @@ const userRouter = require('./routes/userRoutes');
 app.use(morgan('dev'));
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+module.exports = app;
